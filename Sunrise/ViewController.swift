8b38ec021334235c1a5fb09d132f51ecb3be2169
[stringdoth] <[bharathb9595@gmail.com]>
2018-11-15 15:54:03 +0530

    Initial Commit

---
+++ Sunrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunrise
+//
+//  Created by Bharath  Raj kumar on 15/11/18.
+//  Copyright © 2018 Incend Digital Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
